a2a494340bf2d76b3cb00498364fffca120efd53
Wang Zhang <zw199006@gmail.com>
2020-08-15 23:43:06 +0800

    passing test for mlc_tensor_data

--- mlcompute_basic/mlcompute-bridge.mm
+++ mlcompute_basic/mlcompute-bridge.mm
@@ -5,11 +5,6 @@
 #include <iostream>
 #include <vector>
 #include <string>
-#include <assert.h>
-#include <stdlib.h>
-
-#include <algorithm>
-#include <iterator>
 
 #import <Foundation/Foundation.h>
 #import <MLCompute/MLCompute.h>
@@ -40,6 +35,11 @@ MLCTensor* CreateMLCTensor(std::vector<int> &shape_vec, MLCDataType dtype) {
 template<typename T>
 MLCTensorData* CreateMLCTensorData(std::vector<T> &data) {
     return [MLCTensorData dataWithImmutableBytesNoCopy:data.data() length:data.size()*sizeof(T)];
+}
+
+template<typename T>
+MLCTensorData* CreateMLCTensorData(T* data, size_t len) {
+    return [MLCTensorData dataWithImmutableBytesNoCopy:data length:len*sizeof(T)];
 }
 
 template<typename T>
@@ -92,82 +92,19 @@ struct mlc_tensor {
 struct mlc_tensor_data {
     MLCTensorData * core;
 
-    mlc_tensor_data(std::vector<Float32> data) {
-        core = CreateMLCTensor<Float32>(data);
+    mlc_tensor_data(py::array_t<Float32> data) {
+        size_t len = data.size();
+        auto data_info = data.request();
+
+        Float32* data_array = (Float32*)data_info.ptr;
+
+        core = CreateMLCTensorData<Float32>(data_array, len);
     }
 
     void print(void) {
         PrintTensor<Float32>(core);
     }
-}
-
-class mlc_layer {
-public:
-    MLCLayer * base_core;
-    virtual MLCLayer * core_ptr(void);
-    MLCLayer * base_core_ptr(void) {return base_core;}
-}
-
-class mlc_arithmetic_layer {
-public:
-    MLCArithmeticLayer * core;
-
-    mlc_arithmetic_layer(const std::string & operation) {
-        if (operation == "add") {
-            core = [MLCArithmeticLayer layerWithOperation:MLCArithmeticOperationAdd];
-            base_core = (MLCLayer *)core;
-        }
-    }
-
-    MLCLayer * core_ptr(void) {
-        return core;
-    }
-}
-
-struct mlc_graph {
-    MLCGraph * core;
-
-    mlc_graph() {
-        core = [MLCGraph graph];
-    }
-
-    mlc_tensor add_layer(mlc_layer& layer, std::list<mlc_tensor&> py_sources) {
-        NSArray<MLCTensor *> * tensor_array = to_nsarray<mlc_tensor, MLCTensor>(py_sources);
-        MLCTensor * out = [core nodeWithLayer:layer.core_ptr(), sources: tensor_array];
-        return mlc_tensor(out);
-    }
-
-}
-
-struct mlc_interrence_graph {
-    MLCInferenceGraph * core;
-
-    mlc_interrence_graph(std::list<mlc_graph&> py_graphs) {
-
-        NSArray<MLCGraph *> * graph_array = to_nsarray<mlc_graph, MLCGraph>(py_graphs);
-        core = [MLCInferenceGraph graphWithGraphObjects:graph_array];
-    }
-
-    void add_inputs(py::dict inputs_dict) {
-        NSMutableDictionary *inputs = [[NSMutableDictionary alloc] init];
-        for (auto item : inputs_dict) {
-            [inputs setObject:item.second.core forKey:[NSString stringWithUTF8String:item.first.c_str()]]
-        }
-        [core addInputs:[inputs copy]];
-    }
-
-    bool compile(const mlc_device& device) {
-        return [i compileWithOptions:MLCGraphCompilationOptionsNone device:device.core];
-    }
-
-    bool execute(py::dict input_data, int batch_size) {
-        NSMutableDictionary *inputs = [[NSMutableDictionary alloc] init];
-        for (auto item : inputs_dict) {
-            [inputs setObject:item.second.core forKey:[NSString stringWithUTF8String:item.first.c_str()]]
-        }
-        return [core executeWithInputsData:[inputs copy] batchSize:base_core_ptr options:MLCExecutionOptionsSynchronous completionHandler:nil];
-    }
-}
+};
 
 struct mlc_device {
     MLCDevice *core;
@@ -180,7 +117,75 @@ struct mlc_device {
             core = [MLCDevice deviceWithType:MLCDeviceTypeGPU];
         }
     }
-}
+};
+
+// class mlc_layer {
+// public:
+//     MLCLayer * base_core;
+//     virtual MLCLayer * core_ptr(void);
+//     MLCLayer * base_core_ptr(void) {return base_core;}
+// };
+
+// class mlc_arithmetic_layer {
+// public:
+//     MLCArithmeticLayer * core;
+
+//     mlc_arithmetic_layer(const std::string & operation) {
+//         if (operation == "add") {
+//             core = [MLCArithmeticLayer layerWithOperation:MLCArithmeticOperationAdd];
+//             base_core = (MLCLayer *)core;
+//         }
+//     }
+
+//     MLCLayer * core_ptr(void) {
+//         return core;
+//     }
+// };
+
+// struct mlc_graph {
+//     MLCGraph * core;
+
+//     mlc_graph() {
+//         core = [MLCGraph graph];
+//     }
+
+//     mlc_tensor add_layer(mlc_layer& layer, std::list<mlc_tensor&> py_sources) {
+//         NSArray<MLCTensor *> * tensor_array = to_nsarray<mlc_tensor, MLCTensor>(py_sources);
+//         MLCTensor * out = [core nodeWithLayer:layer.core_ptr(), sources: tensor_array];
+//         return mlc_tensor(out);
+//     }
+
+// };
+
+// struct mlc_interrence_graph {
+//     MLCInferenceGraph * core;
+
+//     mlc_interrence_graph(std::list<mlc_graph&> py_graphs) {
+
+//         NSArray<MLCGraph *> * graph_array = to_nsarray<mlc_graph, MLCGraph>(py_graphs);
+//         core = [MLCInferenceGraph graphWithGraphObjects:graph_array];
+//     }
+
+//     void add_inputs(py::dict inputs_dict) {
+//         NSMutableDictionary *inputs = [[NSMutableDictionary alloc] init];
+//         for (auto item : inputs_dict) {
+//             [inputs setObject:item.second.core forKey:[NSString stringWithUTF8String:item.first.c_str()]]
+//         }
+//         [core addInputs:[inputs copy]];
+//     }
+
+//     bool compile(const mlc_device& device) {
+//         return [i compileWithOptions:MLCGraphCompilationOptionsNone device:device.core];
+//     }
+
+//     bool execute(py::dict input_data, int batch_size) {
+//         NSMutableDictionary *inputs = [[NSMutableDictionary alloc] init];
+//         for (auto item : inputs_dict) {
+//             [inputs setObject:item.second.core forKey:[NSString stringWithUTF8String:item.first.c_str()]]
+//         }
+//         return [core executeWithInputsData:[inputs copy] batchSize:base_core_ptr options:MLCExecutionOptionsSynchronous completionHandler:nil];
+//     }
+// };
 
 
 PYBIND11_MODULE(mlcompute, mlc) {
@@ -192,6 +197,9 @@ PYBIND11_MODULE(mlcompute, mlc) {
         .def("print", &mlc_tensor::print);
 
     py::class_<mlc_tensor_data>(mlc, "mlc_tensor_data")
-        .def(py::init<std::vector<Float32>>())
+        .def(py::init<py::array_t<Float32>>())
         .def("print", &mlc_tensor_data::print);
+
+    py::class_<mlc_device>(mlc, "mlc_device")
+        .def(py::init<const std::string &>());
 }
