ff1110a0bedb1152c5dcc15e0316b7631cd60946
Wang Zhang <zw199006@gmail.com>
2020-08-15 16:09:43 +0800

    add mlc_tensor

--- mlcompute_basic/mlcompute-bridge.mm
+++ mlcompute_basic/mlcompute-bridge.mm
@@ -1,7 +1,9 @@
 #include <pybind11/pybind11.h>
+#include <pybind11/stl.h>
 #include <pybind11/numpy.h>
 
 #include <iostream>
+#include <vector>
 #include <string>
 #include <assert.h>
 #include <stdlib.h>
@@ -10,8 +12,7 @@
 #include <iterator>
 
 #import <Foundation/Foundation.h>
-#import <Metal/Metal.h>
-#import <ComputingMetal/ComputingMetal.h>
+#import <MLCompute/MLCompute.h>
 
 namespace py = pybind11;
 
@@ -54,55 +55,28 @@ void PrintTensor(MLCTensor *t) {
     std::cout << std::endl;
 }
 
+
+
 struct mlc_tensor {
-    MLCTensor *
+    MLCTensor * core;
 
-    objcpp_test() {
-        buff_a = nullptr;
-        buff_b = nullptr;
-        buff_c = nullptr;
-        len_a = 0;
-        len_b = 0;
-        len_c = 0;
+    mlc_tensor(std::vector<int> shape, const std::string &dtype) {
+        if (dtype != "float32" && dtype != "Float32") {
+            std::cout << "warning: only float32 is supported, converting to float32" << std::endl;
+        }
 
-        device = MTLCreateSystemDefaultDevice();
-
-        adder = [[MetalAdder alloc] initWithDevice:device];
+        core = CreateMLCTensor(shape, MLCDataTypeFloat32);
     }
 
-    void print_device() {
-        NSLog(@"Using device: %@\n", device.name);
-        return;
+    void print(void) {
+        PrintTensor<Float32>(core);
     }
-
-    void run_metal(py::array_t<float> a, py::array_t<float> b, py::array_t<float> r) {
-        unsigned int len = a.size();
-
-        assert(a.size() == b.size());
-        assert(a.size() == r.size());
-
-        auto a_info = a.request();
-        auto b_info = b.request();
-        auto r_info = r.request();
-
-        float* array_a = (float*)a_info.ptr;
-        float* array_b = (float*)b_info.ptr;
-        float* array_r = (float*)r_info.ptr;
-
-        [adder sendComputeCommand:len array_a:array_a array_b:array_b array_r:array_r];
-        
-        NSLog(@"Execution finished.");
-    }
-
-
-    
 };
 
-PYBIND11_MODULE(mlcompute-cpp-bridge, m) {
-    m.doc() = "pybind11 objective-c++ mixing test";
+PYBIND11_MODULE(mlcompute, mlc) {
+    mlc.doc() = "pybind11 objective-c++ mixing test";
 
-    py::class_<objcpp_test>(m, "objcpp_test")
-        .def(py::init<>())
-        .def("print_device", &objcpp_test::print_device)
-        .def("run_metal", &objcpp_test::run_metal);
+    py::class_<mlc_tensor>(ml, "mlc_tensor")
+        .def(py::init<std::vector<int>, const std::string &>())
+        .def("print", &mlc_tensor::print);
 }
